fb7fa0bd13997240d73016a2730fb33c22873900
James Devarie <jamesdevarie458@gmail.com>
2025-12-16 20:01:51 -0500

    Version 2

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Number-Guessing-Game
+
+This a basic number guessing game between the user and the computer.
+
+The user is introduced and is asked to enter their name,
+
+then the program welcomes the user then prompts for a number between 1 through 10.
+
+The program stores the number in a variable and then generates its own number.
--- main.go
+++ main.go
@@ -6,25 +6,43 @@ import (
 	"time"
 )
 
+// These two variables are outside any function because they were not origianlly working inside a function.
 var userGuess int
 var secretNumber int
+var name string
 
+// Main funtction to just call all the other functions.
 func main() {
+	intro()
 	prompts()
 	randomNumber()
-	checkGuess(userGuess, secretNumber)
+	checkGuess()
+	tryAgain()
 }
 
-func prompts() {
-	fmt.Println("Hello, please enter your name: ")
-	var name string
+func intro() {
+	fmt.Println("Welcome to the number guessing game!")
+	fmt.Println("What is your name?: ")
 	fmt.Scanln(&name)
 	fmt.Println("Welcome", name)
-	fmt.Println("Please guess a number between 1 and 10: ")
-	var userGuess int
-	fmt.Scanln(&userGuess)
 }
 
+/*
+	 The function prompts gets the user's name and their guess for a number between 1 and 10.
+		The number is then stored in the userGuess variable.
+*/
+func prompts() {
+	fmt.Println("Please guess a number between 1 and 10: ")
+	fmt.Scanln(&userGuess)
+	if userGuess < 1 || userGuess > 10 {
+		fmt.Println("Invalid Guess.")
+		prompts()
+	} else {
+		fmt.Println("You guessed", userGuess)
+	}
+}
+
+// The function randomNumber generates a random number between 1 and 10 and sores it in the secretNumber variable.
 func randomNumber() {
 
 	rand.Seed(time.Now().UnixNano())
@@ -32,12 +50,35 @@ func randomNumber() {
 
 }
 
-func checkGuess(userGuess int, secretNumber int) {
+// The function checkGuess compares the user's guess to the secret number provides the user feedback.
+func checkGuess() {
 	if userGuess == secretNumber {
 		fmt.Println("Congratulations! You guessed the correct number.")
 	} else if userGuess < secretNumber {
 		fmt.Println("Too low! The secret was", secretNumber)
-	} else {
+	} else if userGuess > secretNumber {
 		fmt.Println("Too high! The secret was", secretNumber)
+	} else {
+		fmt.Println("LOL")
+	}
+}
+
+// The function tryAgain asks the user if they want to try again and if so, calls the other functions again.
+func tryAgain() {
+	var response string
+
+	fmt.Println("Would you like to try again? (yes/no)")
+	fmt.Scanln(&response)
+
+	if response == "yes" {
+		prompts()
+		randomNumber()
+		checkGuess()
+		tryAgain()
+	} else if response == "no" {
+		fmt.Println("Thanks for playing", name)
+	} else {
+		fmt.Println("Invalid response. Please enter 'yes' or 'no'")
+		tryAgain()
 	}
 }
